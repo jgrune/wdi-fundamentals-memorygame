21f10a6f2352459ba81c345c9c80c1be9dca7af4
Jonathan Grunewald <jonathan.grunewald1@gmail.com>
2016-10-01 13:00:40 -0400

    created game logic

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,1 +1,8 @@
 console.log("JS file is connected to HTML! Woo!")
+
+//defining four card variables for the memory card game
+var cardOne = "queen";
+var cardTwo = "king";
+var cardThree = "queen";
+var cardFour = "king";
+
