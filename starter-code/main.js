97239d935611237b61035e277b71be45f3d544ab
Jonathan Grunewald <jonathan.grunewald1@gmail.com>
2016-10-02 14:56:04 -0400

    updated main.js with game logic, copied html and css files to this directory

--- starter-code/main.js
+++ starter-code/main.js
@@ -6,8 +6,30 @@ var cardTwo = "king";
 var cardThree = "queen";
 var cardFour = "king";
 
+/*
 if (cardOne === cardTwo){
 	alert("You found a match!");
 }	else {
 		alert("Sorry, try again!");
 	}
+	*/
+
+//initial declaration of total card variable
+var gameBoard = document.querySelector('#game-board');
+var totalCards = 4;
+
+//function to create cards with 'card' class
+var createCards = function() {
+	for (x=0; x < totalCards; x++) {
+		var newCard = document.createElement('div');
+		newCard.className = 'card';
+		gameBoard.appendChild(newCard);
+	}	
+
+}
+
+//connect create card function to button
+
+var cardButton = document.getElementById('cardButton');
+
+cardButton.addEventListener('click', createCards);
